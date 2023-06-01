214fa7c001a2d479dd1b88ecf13d3061fa661930
Almıla <almilaoztaskin@gmail.com>
2023-06-01 04:50:45 +0300

    Düzeltme Yapıldı.

---
+++ AspNetMvcBlog/App.Web.Mvc/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+	public class BlogController : Controller
+	{
+		public IActionResult Search()
+		{
+			return View();
+		}
+		public IActionResult Detail()
+		{
+			return View();
+		}
+	}
+}
